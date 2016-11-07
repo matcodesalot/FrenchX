845c15bf2d499e79e0a7bfe107039917d9acace1
Beatrix <bealearnscode@gmail.com>
2016-11-07 18:22:43 +0000

    add div to html index. install dependencies. create App component.

---
+++ client/js/components/App.js
@@ -0,0 +1,12 @@
+var React = require("react");
+
+//make app function that renders jsx element
+var App = function(){
+    return (
+    <div>
+        <h1>French X</h1>
+    </div>
+    )
+}
+
+module.exports = App;
--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,12 @@
-import 'babel-polyfill';
+require('babel-polyfill');
+var React = require("react");
+var ReactDOM = require("react-dom");
+var App = require("./components/App");
+
+document.addEventListener("DOMContentLoaded", function() {
+    ReactDOM.render(<App />,document.getElementById("app"));
+});
+
+
 
 console.log(`Client running in ${process.env.NODE_ENV} mode`);
