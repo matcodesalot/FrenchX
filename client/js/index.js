d2bfd82aabc75e6151c01e36d1d1616921a9b0b5
Beatrix <bealearnscode@gmail.com>
2016-11-08 17:23:28 +0000

    added thunk, middleware, and others

--- client/js/index.js
+++ client/js/index.js
@@ -5,7 +5,18 @@ var App = require("./components/App");
 var store = require("./store");
 import { Provider } from 'react-redux';
 
+var questionReducer = require('./reducers/reducers');
+var createStore = require('redux').createStore;
+var actions = require('./action/actions');
+var applyMiddleware = require('redux').applyMiddleware;
+var thunk = require('redux-thunk');
+
+var store = createStore(questionReducer);
+
 
 document.addEventListener("DOMContentLoaded", function() { 
 	ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("app")); 
 });
+
+
+module.exports = store;
