ba6db2d3d56f56d44ed880f65089dbac380ba2aa
Beatrix <bealearnscode@gmail.com>
2016-11-09 17:39:15 +0000

    created homepage component

---
+++ client/js/components/Homepage.js
@@ -0,0 +1,16 @@
+var React = require("react");
+var connect = require("react-redux").connect;
+var RegisterLogIn = require("./RegisterLogIn");
+
+var Homepage = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <h1>French X</h1>
+                <h2>Learn Languages Through Spaced Repetition</h2>
+                <RegisterLogIn />
+            </div>
+        );
+    }
+
+});
