267bad41a1a4a375470de382c3bbf6883994eb7e
Beatrix <bealearnscode@gmail.com>
2016-11-10 03:26:53 +0000

    surrounded text in addition div for design purposes. will probably remove.

--- client/js/components/Homepage.js
+++ client/js/components/Homepage.js
@@ -10,11 +10,13 @@ var Homepage = React.createClass({
     render: function(){
         return (
             <div id = "homepage">
-                <h1>French X</h1>
-                <h2>Learn Languages Through Spaced Repetition</h2>
+                <div id ="homepage-text">
+                    <h1>French X</h1>
+                    <h2>Learn Languages Through Space Repetition</h2>
+                </div>
                 <div id = "register-login">
                 	<button id = "register-login-button" ><Link to="/home">Register/Log In with Google</Link></button>
-            	</div>
+                </div>
             </div>
         );
     }
