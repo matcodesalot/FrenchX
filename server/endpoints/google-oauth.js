d929ea8c0d2923579987e31bcb1a7426ef9ff316
mat <reallymathew@gmail.com>
2016-11-10 11:42:23 -0600

    thanks lavie

--- server/endpoints/google-oauth.js
+++ server/endpoints/google-oauth.js
@@ -7,12 +7,17 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const googleRouter = express.Router();
 
-passport.use(new GoogleStrategy({
-	clientID: secrets.clientId,
-	clientSecret: secrets.clientSecret,
-	callbackURL: "http://" + secrets.devHostName + "/auth/google/callback"
+passport.use(new GoogleStrategy(
+	{
+		clientID: secrets.clientID,
+		clientSecret: secrets.clientSecret,
+		callbackURL: "http://" + secrets.devHostName + "/auth/google/callback",
+		passReqToCallback: true
+
 	},
-	function(accessToken, refreshToken, profile, cb) {
+	function(request, accessToken, refreshToken, profile, cb) {
+		request.accessToken = accessToken;
+
 		User.findOrCreate(profile.id, function(err, user) {
 			return cb(err, user);
 		});
@@ -21,12 +26,14 @@ passport.use(new GoogleStrategy({
 
 googleRouter.use(passport.initialize());
 
+
 googleRouter.get('/', passport.authenticate('google', {scope: ['profile'], session: false}));
 
 googleRouter.get('/callback', passport.authenticate('google', {failureRedirect: '/login', session: false}),
 	function(req, res) {
 		//successful authentication, redirect home
-		res.redirect('/');
+		console.log('req.accessToken', req.accessToken);
+		res.redirect('/?auth=' + req.accessToken);
 	}
 );
 
