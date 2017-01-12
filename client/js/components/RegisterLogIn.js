b0bd37c05ac2809f56d46756ecd0b0b31db62283
LavieMaru <laviemaru@gmail.com>
2017-01-11 23:12:40 -0600

    remove comments

--- client/js/components/RegisterLogIn.js
+++ client/js/components/RegisterLogIn.js
@@ -2,7 +2,6 @@ var React = require("react");
 var Link = require('react-router').Link;
 import ActionAndroid from 'material-ui/svg-icons/action/android';
 import RaisedButton from 'material-ui/RaisedButton';
-
 const FontAwesome = require('react-fontawesome');
 
 export default () => {
@@ -16,18 +15,4 @@ export default () => {
 		    />
 		</div>
 	)
-}
-
-// var RegisterLogIn = React.createClass({
-
-//     render: function(){
-//         return (
-//             <div id = "register-login">
-//                 <button id = "register-login-button" ><Link to="/home">Register/Log In with Google</Link></button>
-//             </div>
-//         );
-//     }
-
-// });
-
-// module.exports = RegisterLogIn;
+};
