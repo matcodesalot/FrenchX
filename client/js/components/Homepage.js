75a941cd645c26dbd85b6a84c66e1ce42d9ad9e3
Lavie Ruan <lavieruan@Lavies-MacBook.local>
2017-01-16 19:33:51 -0600

    readd register login component back into parent component

--- client/js/components/Homepage.js
+++ client/js/components/Homepage.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import HomepageText from './Title';
-import RegisterLogin from './RegisterLogin';
+import RaisedButton from 'material-ui/RaisedButton';
+const FontAwesome = require('react-fontawesome');
 
 export default () => {
     return (
         <MuiThemeProvider>
             <div id = "homepage">
                 <HomepageText />
-                <RegisterLogin />
+                <div id = "register-login">
+                    <RaisedButton
+                        href="/auth/google"
+                        label={"Sign in with"}
+                        labelPosition="before"
+                        icon={<FontAwesome name='google'/>}
+                    />
+                </div>
             </div>
         </MuiThemeProvider>
     )
